11ca891c798428f465ffbe98b998bc4bdf61f2a6
Khalil Ramzi <khalilramzi6@gmail.com>
2024-01-08 13:22:11 -0500

    Updated Member class

--- Member.java
+++ Member.java
@@ -1,5 +1,6 @@
 package EuchreCodingProject;
 
+import java.util.ArrayList;
 public class Member {
     private String name;
     private String username; 
@@ -10,4 +11,29 @@ public class Member {
     private int winsOnOffence;
     private int winsOnDefence;
     private int lossesOnOffence;
+    private int lossesOnDefence;
+    private float winLossRate;
+    private float averageScore;
+    private ArrayList<Tournament> tournaments;
+    private ArrayList<Game> gamesPlayed;
+    private ArrayList<Partnership> partnershipHistory;
+
+    // Setup multiple overloaded constructors for varying amounts of data available when addMember is called
+    public Member(String name, String username, long userID, int numGames, int totalWins, int totalLosses, int winsOnOffence, int winsOnDefence, int lossesOnOffence, int lossesOnDefence, float winLossRate, float averageScore) {
+        this.name = name;
+        this.username = username;
+        this.userID = userID;
+        this.numGames = numGames;
+        this.totalWins = totalWins;
+        this.totalLosses = totalLosses;
+        this.winsOnOffence = winsOnOffence;
+        this.winsOnDefence = winsOnDefence;
+        this.lossesOnOffence = lossesOnOffence; 
+        this.lossesOnDefence = lossesOnDefence;
+        this.winLossRate = winLossRate;
+        this.averageScore = averageScore; 
+        this.tournaments = new ArrayList<Tournament>();
+        this.gamesPlayed = new ArrayList<Game>();
+        this.partnershipHistory = new ArrayList<Partnership>();
+    }
 }
