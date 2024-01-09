9e41019c65226ea54b3823965135e7f1b5811aa7
Khalil Ramzi <khalilramzi6@gmail.com>
2024-01-09 10:21:46 -0500

    Updated getters and setters for Member

--- Member.java
+++ Member.java
@@ -5,6 +5,7 @@ public class Member {
     private String name;
     private String username; 
     private long userID;
+    private long userIDGen = 1000000; 
     private int numGames;
     private int totalWins;
     private int totalLosses;
@@ -38,4 +39,56 @@ public class Member {
         this.gamesPlayed = gamesPlayed;
         this.partnershipHistory = partnershipHistory;
     }
+
+    // This constructor is for a brand new member
+    public Member(String name, String username) {
+        this.name = name;
+        this.username = username;
+        this.userID = userIDGen;
+        userIDGen++;
+        this.numGames = 0;
+        this.totalWins = 0;
+        this.totalLosses = 0;
+        this.winsOnOffence = 0;
+        this.winsOnDefence = 0;
+        this.lossesOnOffence = 0; 
+        this.lossesOnDefence = 0;
+        this.winLossRate = 0;
+        this.averageScore = 0; 
+        this.tournaments = new ArrayList<Tournament>();
+        this.gamesPlayed = new ArrayList<Game>();
+        this.partnershipHistory = new ArrayList<Partnership>();
+    }
+
+    public String getName() {return name;}
+    public String getUsername() {return username;}
+    public long getUserID() {return userID;}
+    public int getNumGames() {return numGames;}
+    public int getTotalWins() {return totalWins;}
+    public int getTotalLosses() {return totalLosses;}
+    public int getWinsOnOffence() {return winsOnOffence;}
+    public int getWinsOnDefence() {return winsOnDefence;}
+    public int getLossesOnOffence() {return lossesOnOffence;}
+    public int getLossesOnDefence() {return lossesOnDefence;}
+    public float getWinLossRate() {return winLossRate;}
+    public float getAverageScore() {return averageScore;}
+    public ArrayList<Tournament> getTournaments() {return tournaments;}
+    public ArrayList<Game> getGamesPlayed() {return gamesPlayed;}
+    public ArrayList<Partnership> getPartnershipHistory() {return partnershipHistory;}
+
+    public void setName(String name) {this.name = name;}
+    public void setUsername(String username) {this.username = username;}
+    public void newGame(boolean win, boolean offence, int score) {
+        numGames++;
+        if (win) {
+            totalWins++;
+            if (win) {winsOnOffence++;} else {winsOnDefence++;}
+        } else {
+            totalLosses++;
+            if (offence) {lossesOnOffence++;} else {lossesOnDefence++;}
+        }
+        winLossRate = totalWins / numGames; 
+        averageScore = (averageScore*(numGames-1) + score)/numGames;
+    } 
+
 }
