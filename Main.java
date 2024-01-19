1ab03668407d7dd67819015c7ec46076eb20dde7
Khalil Ramzi <khalilramzi6@gmail.com>
2024-01-19 10:58:01 -0500

    Random

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ package EuchreCodingProject;
 public class Main {
     public static void main(String args[]) {
         Statistics statistics = new Statistics();
+        Member member1 = new Member("KR", "darkr");
         
         System.out.println("hello world");
     }
