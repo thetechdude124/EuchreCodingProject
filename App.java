45c55ce3363f87523c38f47c556c26ea557030b1
J-D9 <105740642+J-D9@users.noreply.github.com>
2024-01-21 17:21:33 -0500

    more jfx

--- App.java
+++ App.java
@@ -1,27 +1,36 @@
-import java.awt.Color;
-
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.input.KeyCombination;
 import javafx.stage.Stage;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
 
 public class App extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
+        // Setups
         Group root = new Group();
-        Scene scene = new Scene(root);
+        Scene scene = new Scene(root, Color.BLACK);
         
+        // Icon & title
         Image icon = new Image("among us.png");
         stage.getIcons().add(icon);
-
         stage.setTitle("among us");
 
-        stage.setScene(scene);
+        // Screen size & position
+        stage.setWidth(420);
+        stage.setWidth(420);
+        stage.setResizable(false);
+        stage.setX(50);
+        stage.setY(50);
+        stage.setFullScreen(true);
+        stage.setFullScreenExitHint("F for unfullscreen");
+        stage.setFullScreenExitKeyCombination(KeyCombination.valueOf("f"));
 
+        // Display code
+        stage.setScene(scene);
         stage.show();
     }
 
