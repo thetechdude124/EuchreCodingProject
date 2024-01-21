b2ba462dca03280a43b7c40f1bae4d31264aa6f9
darkr-code <khalilramzi6@gmail.com>
2024-01-20 20:25:33 -0500

    eliminating error messages

--- App.java
+++ App.java
@@ -1,3 +1,4 @@
+package EuchreCodingProject;
 import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
--- Main.java
+++ Main.java
@@ -1,9 +1,6 @@
 package EuchreCodingProject;
 public class Main {
     public static void main(String args[]) {
-        Statistics statistics = new Statistics();
-        Member member1 = new Member("KR", "darkr");
-        
         System.out.println("hello world");
     }
 }
