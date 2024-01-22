c692499f34d9785a1bf88ebb670f0a2522be930b
darkr-code <khalilramzi6@gmail.com>
2024-01-22 10:49:14 -0500

    Upgraded statistics

--- Node.java
+++ Node.java
@@ -1,31 +1,19 @@
 public class Node {
     private int ID;
-    private Member member;
-    private Partnership partnership;
     private Node previous;
     private Node next; 
 
-    public Node(int ID, Member member, Node previous, Node next) {
+    public Node(int ID, Node previous, Node next) {
         this.ID = ID;
-        this.member = member;
-        this.previous = previous;
-        this.next = next;
-    }
-
-    public Node(int ID, Partnership partnership, Node previous, Node next) {
-        this.ID = ID;
-        this.partnership = partnership;
         this.previous = previous;
         this.next = next;
     }
 
     public int getID() {return ID;}
-    public Member getMember() {return member;}
-    public Partnership getPartnership() {return partnership;}
     public Node getPrevious() {return previous;}
     public Node getNext() {return next;}
 
-    public void setData(Member member) {this.member = member;}
+    public void setID(int ID) {this.ID = ID;}
     public void setPrevious(Node previous) {this.previous = previous;}
     public void setNext(Node next) {this.next = next;}
 }
--- Statistics.java
+++ Statistics.java
@@ -1,105 +1,94 @@
-import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Statistics {
     // Below are custom LinkedLists referred to by their head
     // All the members and partnerships are included in each of the LinkedLists, the purpose of the duplicates is to store sorted members for each stat 
     // There is an array that contains all the stat lists for each member and partnership
-    private Node membersByNumGames;          //index0
-    private Node membersByWins;              //index1
-    private Node membersByLosses;            //index2
-    private Node membersByOWins;             //index3
-    private Node membersByDWins;             //index4
-    private Node membersByOLosses;           //index5
-    private Node membersByDLosses;           //index6
-    private Node membersByWLRate;            //index7
-    private Node membersByAvgScore;          //index8
-    private ArrayList<Node> memberStats;
-    private Node partnershipsByNumGames;     //index0
-    private Node partnershipsByWins;         //index1
-    private Node partnershipsByLosses;       //index2
-    private Node partnershipsByOWins;        //index3
-    private Node partnershipsByDWins;        //index4
-    private Node partnershipsByOLosses;      //index5
-    private Node partnershipsByDLosses;      //index6
-    private Node partnershipsByWLRate;       //index7
-    private Node partnershipsByAvgScore;     //index8
-    private ArrayList<Node> partnershipStats;
+    private HashMap<Integer, Member> allMembers;
+    private Node[] memberStats;          // holds different ranking lists at each index, specified below
+    /*
+    membersByNumGames           //index0
+    membersByWins               //index1
+    membersByLosses             //index2
+    membersByOWins              //index3
+    membersByDWins              //index4
+    membersByOLosses;           //index5
+    membersByDLosses;           //index6
+    membersByWLRate;            //index7
+    membersByAvgScore;          //index8
+    */
+    private HashMap<Integer, Partnership> allPartnerships;
+    private Node[] partnershipStats;    // holds different ranking lists at each index, specified below 
+    /*
+    partnershipsByNumGames;     //index0
+    partnershipsByWins;         //index1
+    partnershipsByLosses;       //index2
+    partnershipsByOWins;        //index3
+    partnershipsByDWins;        //index4
+    partnershipsByOLosses;      //index5
+    partnershipsByDLosses;      //index6
+    partnershipsByWLRate;       //index7
+    partnershipsByAvgScore;     //index8
+    */
     private Node node;
 
     public Statistics() {
-        this.membersByNumGames = new Node(0, null, null, null);
-        this.membersByWins = new Node(0, null, null, null);
-        this.membersByLosses = new Node(0, null, null, null);
-        this.membersByOWins = new Node(0, null, null, null);
-        this.membersByDWins = new Node(0, null, null, null);
-        this.membersByOLosses = new Node(0, null, null, null);
-        this.membersByDLosses = new Node(0, null, null, null);
-        this.membersByWLRate = new Node(0, null, null, null);
-        this.membersByAvgScore = new Node(0, null, null, null);
-        this.memberStats = new ArrayList<Node>();
-        memberStats.add(membersByNumGames);
-        memberStats.add(membersByWins);
-        memberStats.add(membersByLosses);
-        memberStats.add(membersByOWins);
-        memberStats.add(membersByDWins);
-        memberStats.add(membersByOLosses);
-        memberStats.add(membersByDLosses);
-        memberStats.add(membersByWLRate);
-        memberStats.add(membersByAvgScore);
-        this.partnershipsByNumGames = new Node(0, null, null, null);
-        this.partnershipsByWins = new Node(0, null, null, null);
-        this.partnershipsByLosses = new Node(0, null, null, null);
-        this.partnershipsByOWins = new Node(0, null, null, null);
-        this.partnershipsByDWins = new Node(0, null, null, null);
-        this.partnershipsByOLosses = new Node(0, null, null, null);
-        this.partnershipsByDLosses = new Node(0, null, null, null);
-        this.partnershipsByWLRate = new Node(0, null, null, null);
-        this.partnershipsByAvgScore = new Node(0, null, null, null);
-        this.partnershipStats = new ArrayList<Node>();
-        partnershipStats.add(partnershipsByNumGames);
-        partnershipStats.add(partnershipsByWins);
-        partnershipStats.add(partnershipsByLosses);
-        partnershipStats.add(partnershipsByOWins);
-        partnershipStats.add(partnershipsByDWins);
-        partnershipStats.add(partnershipsByOLosses);
-        partnershipStats.add(partnershipsByDLosses);
-        partnershipStats.add(partnershipsByWLRate);
-        partnershipStats.add(partnershipsByAvgScore);
+        this.allMembers = new HashMap<Integer, Member>();
+        this.memberStats = new Node[9];
+
+        this.allPartnerships = new HashMap<Integer, Partnership>();
+        this.partnershipStats = new Node[9];
     }
 
-    public Object getMember(int userID) {
-        node = membersByNumGames;
-        while (node.getID() != userID && node.getNext() != null) {node = node.getNext();}
-        if (node.getNext() == null) {return null;}
-        return node.getMember();
+    public Member getMember(int userID) {
+        if (allMembers.containsKey(userID)) {
+            return allMembers.get(userID);
+        }
+        return null;
     }
 
-    public Object getPartnership(int partnershipID) {
-        node = membersByNumGames;
-        while (node.getID() != partnershipID && node.getNext() != null) {node = node.getNext();}
-        if (node.getNext() == null) {return null;}
-        return node.getPartnership();
+    public Partnership getPartnership(int userID) {
+        if (allMembers.containsKey(userID)) {
+            return allPartnerships.get(userID);
+        }
+        return null;
+    }
+
+    public void addMember(Member member) {
+        allMembers.put(member.getUserID(), member);
+        for (int i = 0; i <= 9; i++) {
+            memberStats[i] = new Node(member.getUserID(), null, memberStats[i]);
+        }
+    }
+
+    public void addPartnership(Partnership partnership) {
+        allPartnerships.put(partnership.getUserID(), partnership);
+        for (int i = 0; i <= 9; i++) {
+            partnershipStats[i] = new Node(partnership.getUserID(), null, partnershipStats[i]);
+        }
     }
 
     public Node getMemberRanksByStat(int stat) {
-        memberStats.set(stat, memberMergeSort(memberStats.get(stat), memberStats.size(), stat));
-        return memberStats.get(stat);
+        memberStats[stat] = memberMergeSort(memberStats[stat], memberStats.length, stat);
+        return memberStats[stat];
     }
 
     public Node getPartnershipRanksByStat(int stat) {
-        partnershipStats.set(stat, partnershipMergeSort(partnershipStats.get(stat), partnershipStats.size(), stat));
-        return partnershipStats.get(stat);
+        partnershipStats[stat] = partnershipMergeSort(partnershipStats[stat], partnershipStats.length, stat);
+        return partnershipStats[stat];
     }
 
-    // This section is for the sorting of any Member LinkedLists
-    public void memberAddToEnd(Node tempNode, int ID, Member data) {  
-        while (tempNode.getNext() != null) {
-            tempNode = tempNode.getNext();
-        } 
-        Node newNode = new Node(ID, data, tempNode, null);
+    private void addToEnd(Node tempNode, int userID) {  
+        if (tempNode != null) {
+            while (tempNode.getNext() != null) {
+                tempNode = tempNode.getNext();
+            } 
+        }
+        Node newNode = new Node(userID, tempNode, null);
         tempNode.setNext(newNode);
     }
 
+    // This section is for the sorting of any Member LinkedLists
     private Node memberMergeSort(Node nodeA, int sortSize, int stat) {
         if (sortSize == 1) {return nodeA;} 
  
@@ -115,31 +104,31 @@ public class Statistics {
     }
 
     private Node memberMerge(Node nodeA, Node nodeB, int stat) {
-        if (nodeA.getMember().getStats()[stat] < nodeB.getMember().getStats()[stat]) {
-            node = new Node(nodeA.getID(), nodeA.getMember(), null, null); 
+        if (allMembers.get(nodeA.getID()).getStats()[stat] < allMembers.get(nodeB.getID()).getStats()[stat]) {
+            node = new Node(nodeA.getID(), null, null); 
             nodeA = nodeA.getNext();
         } else {
-            node = new Node(nodeB.getID(), nodeB.getMember(), null, null);
+            node = new Node(nodeB.getID(), null, null);
             nodeB = nodeB.getNext();
         }
 
         while (nodeA != null && nodeB != null) {
-            if (nodeA.getMember().getStats()[stat] < nodeB.getMember().getStats()[stat]) {
-                memberAddToEnd(node, nodeA.getID(), nodeA.getMember());
+            if (allMembers.get(nodeA.getID()).getStats()[stat] < allMembers.get(nodeB.getID()).getStats()[stat]) {
+                addToEnd(node, nodeA.getID());
                 nodeA = nodeA.getNext();
             } else {
-                memberAddToEnd(node, nodeB.getID(), nodeB.getMember());
+                addToEnd(node, nodeB.getID());
                 nodeB = nodeB.getNext();
             }
         }
 
         while (nodeA != null) {
-            memberAddToEnd(node, nodeA.getID(), nodeA.getMember());
+            addToEnd(node, nodeA.getID());
             nodeA = nodeA.getNext();
         }
 
         while (nodeB != null) {
-            memberAddToEnd(node, nodeB.getID(), nodeB.getMember());
+            addToEnd(node, nodeB.getID());
             nodeB = nodeB.getNext();
         }
 
@@ -147,14 +136,6 @@ public class Statistics {
     }
 
     // This section is for the sorting of any Partnership LinkedLists
-    public void partnershipAddToEnd(Node tempNode, int ID, Partnership data) {  
-        while (tempNode.getNext() != null) {
-            tempNode = tempNode.getNext();
-        } 
-        Node newNode = new Node(ID, data, tempNode, null);
-        tempNode.setNext(newNode);
-    }
-
     private Node partnershipMergeSort(Node nodeA, int sortSize, int stat) {
         if (sortSize == 1) {return nodeA;} 
  
@@ -170,31 +151,31 @@ public class Statistics {
     }
 
     private Node partnershipMerge(Node nodeA, Node nodeB, int stat) {        
-        if (nodeA.getPartnership().getStats()[stat] < nodeB.getPartnership().getStats()[stat]) {
-            node = new Node(nodeA.getID(), nodeA.getPartnership(), null, null); 
+        if (allPartnerships.get(nodeA.getID()).getStats()[stat] < allPartnerships.get(nodeB.getID()).getStats()[stat]) {
+            node = new Node(nodeA.getID(), null, null); 
             nodeA = nodeA.getNext();
         } else {
-            node = new Node(nodeB.getID(), nodeB.getPartnership(), null, null);
+            node = new Node(nodeB.getID(), null, null);
             nodeB = nodeB.getNext();
         }
 
         while (nodeA != null && nodeB != null) {
-            if (nodeA.getPartnership().getStats()[stat] < nodeB.getPartnership().getStats()[stat]) {
-                partnershipAddToEnd(node, nodeA.getID(), nodeA.getPartnership());
+            if (allPartnerships.get(nodeA.getID()).getStats()[stat] < allPartnerships.get(nodeB.getID()).getStats()[stat]) {
+                addToEnd(node, nodeA.getID());
                 nodeA = nodeA.getNext();
             } else {
-                partnershipAddToEnd(node, nodeB.getID(), nodeB.getPartnership());
+                addToEnd(node, nodeB.getID());
                 nodeB = nodeB.getNext();
             }
         }
 
         while (nodeA != null) {
-            partnershipAddToEnd(node, nodeA.getID(), nodeA.getPartnership());
+            addToEnd(node, nodeA.getID());
             nodeA = nodeA.getNext();
         }
 
         while (nodeB != null) {
-            partnershipAddToEnd(node, nodeB.getID(), nodeB.getPartnership());
+            addToEnd(node, nodeB.getID());
             nodeB = nodeB.getNext();
         }
 
